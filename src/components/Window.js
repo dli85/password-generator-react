b3e92ddf4d6c3b7407ab538bfa831976f9dc982f
David Li <dali7758769@gmail.com>
2022-08-17 14:41:33 -0400

    Initial

---
+++ src/components/Window.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Window.css';
+
+const Window = () => {
+    return (
+        <div className="window">
+            <h2 className='window-header'>
+                Password Generator
+            </h2>
+            <div className='generate-result'>
+
+            </div>
+            <button className='rearrange'>Rearrange password</button>
+            <div className='option'>
+                <label className='option-label'># of uppercase letters</label>
+                <input className='option-input-number' type='number' min={0} defaultValue={0}></input>
+            </div>
+            <div className='option'>
+                <label className='option-label'># of lowercase letters</label>
+                <input className='option-input-number' type='number' min={0} defaultValue={0}></input>
+            </div>
+            <div className='option'>
+                <label className='option-label'># of digits</label>
+                <input className='option-input-number' type='number' min={0} defaultValue={0}></input>
+            </div>
+            <div className='option'>
+                <label className='option-label'># of symbols</label>
+                <input className='option-input-number' type='number' min={0} defaultValue={0}></input>
+            </div>
+            <div className='option'>
+                <label className='option-label'>Include phrase?</label>
+                <input className='option-input-text' type='text'></input>
+            </div>
+        </div>
+    );
+};
+
+export default Window;
